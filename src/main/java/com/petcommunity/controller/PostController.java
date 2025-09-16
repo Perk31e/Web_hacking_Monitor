861bad80fbfaa01ca092f529a69e1f83586924c9
LEEWONHUI <OfllineWorld@gmail.com>
2025-09-17 01:05:40 +0900

    delete uuid

--- src/main/java/com/petcommunity/controller/PostController.java
+++ src/main/java/com/petcommunity/controller/PostController.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import java.net.MalformedURLException;
 import org.springframework.core.io.Resource;
